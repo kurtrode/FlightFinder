b26e3fc8120ea93c57bdb020891851ce58d3be6a
MRokaya <rokayamaan@gmail.com>
2023-03-29 12:34:56 +0200

    search

--- resources/js/front/FlightFinder.jsx
+++ resources/js/front/FlightFinder.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 
@@ -48,16 +49,29 @@ return(
 
  
             {   
-                airlines.filter((airline) => {
-                    return (airline.flag == searchQuery || airline.aircraft_icao == searchQuery || airline.departure_airport ? airline.departure_airport.icao_code == searchQuery : null)
-                } ).filter((airline, index) => (index < 5)).map((airline, index)=>
+                // airlines.filter((airline) => {
+                //     return (airline.flag == searchQuery || airline.aircraft_icao == searchQuery || airline.departure_airport ? airline.departure_airport.icao_code == searchQuery : null)
+                // } ).filter((airline, index) => (index < 5)).map((airline, index)=>
                 
+                //flight number, arrival, depa and flight status
+
+                airlines.map((airline, index)=>
                 <ul
                     key= { index }
                     >
+                    <Link to="/">
                     <div className="aircraft_details">
+                        <p>Aircraft type: {airline.flight_icao ? airline.flight_icao
+                        :
+                        "no number"}</p>
+                        <p>Arrival : {airline.arrival_airport.name ? airline.arrival_airport.name
+                        :
+                        "no arrival"}</p>
+                         <p>Dapature: {airline.depature_airport.name ? airline.arrival_airport.name
+                        :
+                        "no depature"}</p>
 
-                        <p>Aircraft type: {airline.aircraft_icao ? airline.aircraft_icao
+                        {/* <p>Aircraft type: {airline.aircraft_icao ? airline.aircraft_icao
                         :
                         "no aircraft"}</p>
                         <p>Status: {airline.status ? airline.status
@@ -82,9 +96,10 @@ return(
                         "no status"}</p>
                         <p>Flag of Airline: {airline.flag ? airline.flag
                         :
-                        "no flag"}</p>
+                        "no flag"}</p> */}
 
                     </div>
+                    </Link>
                 </ul>
                                
                 )
