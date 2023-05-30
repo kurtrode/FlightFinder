2e2f9a8ea26ff0728cf2d826b0a1297fbc320e44
Kurt Rode <kurtrode@seznam.cz>
2023-05-30 10:05:25 +0200

    added database change

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_T_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -92,6 +92,43 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+        'remote' => [
+
+    'driver' => 'mysql',
+
+    'url' => env('REMOTE_DATABASE_URL'),
+
+    'host' => env('REMOTE_DB_HOST', '127.0.0.1'),
+
+    'port' => env('REMOTE_DB_PORT', '3306'),
+
+    'database' => env('REMOTE_DB_DATABASE', 'forge'),
+
+    'username' => env('REMOTE_DB_USERNAME', 'forge'),
+
+    'password' => env('REMOTE_DB_PASSWORD', ''),
+
+    'unix_socket' => env('REMOTE_DB_SOCKET', ''),
+
+    'charset' => 'utf8mb4',
+
+    'collation' => 'utf8mb4_unicode_ci',
+
+    'prefix' => '',
+
+    'prefix_indexes' => true,
+
+    'strict' => true,
+
+    'engine' => null,
+
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+
+    ]) : [],
+
+],
 
     ],
 
